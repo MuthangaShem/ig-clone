5bb8b8e2efe6db23778b8130c17c92da439a5df8
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-12 18:44:49 +0300

    create logout view function

--- insta_app/views.py
+++ insta_app/views.py
@@ -41,3 +41,8 @@ def login_view(request):
             # Failure
             return index(request, auth_form=form)
     return redirect('/')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
