44052534533b50861e5913d290c9d85520df25bd
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-14 00:25:01 +0300

    added routes for users, user profiles and follow

--- insta_app/urls.py
+++ insta_app/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     url(r'^login$', views.login_view, name='login'),  # login
     url(r'^logout$', views.logout_view, name='logout'),  # logout
     url(r'^signup$', views.signup, name='signup'),  # signup
-    url(r'^submit$', views.submit, name='submit'),
+    url(r'^submit$', views.submit, name='submit'),  # submit new post
+    url(r'^users/$', views.users, name='users'),
+    url(r'^users/(?P<username>\w{0,30})/$', views.users),
+    url(r'^follow$', views.follow),
 ]
